fdfee96088c75884143ab1d7cd6ca11702f4ce7f
Fiker Yibeltal <Fikeryibeltal31@gmail.com>
2023-06-19 15:14:08 +0300

    journeyboard alignment

--- src/components/journeyboard.jsx
+++ src/components/journeyboard.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Journeyboard = () => {
     return (  
-        <div className="bg-gray-300 rounded-lg border border-gray-500 shadow-md">
-       <button className="border border-purple-500 rounded-md px-4 py-2 bg-transparent hover:bg-purple-500 hover:text-white transition-colors">
+        <div className="bg-gray-300 rounded-lg border border-gray-500 p-4 shadow-md">
+       <button className="border border-purple-500 rounded-md px-4 py-2 bg-transparent hover:bg-purple-500 hover:text-white transition-colors text-lg">
       1
     </button>
     </div>
