882231f19b2b83a89818db18f1b937e1a1ad3fe4
Fiker Yibeltal <Fikeryibeltal31@gmail.com>
2023-06-19 09:24:15 +0300

    initializing project

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,16 @@
 import styles from "./style";
-import {Navbar,Hero,Stats,Biling,Business,CTA,CardDeal,Testimonials,Footer,Clients} from './components';
+import {Navbar} from './components';
 
 const App = () => (
-    <div className='bg-primary w-full overflow-hidden'>
+    <div className='bg-gray-200 w-full overflow-hidden'>
      <div className={`${styles.paddingX} ${styles.flexCenter} `}>
       <div className={`${styles.boxWidth}`}>
           <Navbar/>
       </div>
      </div>
-     <div className={`bg-primary ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>
-       <Hero/>
-      </div>
-     </div>
-     <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Stats/>
-        <Business/>
-        <Biling/>
-        <CardDeal/>
-        <Testimonials/>
-        <Clients/>
-        <CTA/>
-        <Footer/>
-      </div>
-     </div>
+   
+ 
+      
       </div>
 );
 
