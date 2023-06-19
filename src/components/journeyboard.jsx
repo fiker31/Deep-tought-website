7040cc6cd8e61ee1a46ecdbb8ec2eb36fcde9276
Fiker Yibeltal <Fikeryibeltal31@gmail.com>
2023-06-19 15:31:53 +0300

    journeyboard styling

--- src/components/journeyboard.jsx
+++ src/components/journeyboard.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Journeyboard = () => {
     return (  
-        <div className="bg-gray-300 rounded-lg border border-gray-500 p-4 shadow-md">
-       <button className="border border-purple-500 rounded-md px-4 py-2 bg-transparent hover:bg-purple-500 hover:text-white transition-colors text-lg">
+        <div className="bg-gray-300 rounded-lg p-4 shadow-md">
+       <button className="border border-blue-500 rounded-md px-4 py-2 bg-transparent hover:bg-blue-500 hover:text-white transition-colors text-lg text-color-blue">
       1
     </button>
     </div>
