371e2cddc11fd6628517811af856dcb398f403b5
Fiker Yibeltal <Fikeryibeltal31@gmail.com>
2023-06-22 11:53:00 +0300

    add structure your pointer

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -8,6 +8,11 @@ import CommentIcon from "@mui/icons-material/Comment";
 import LiveHelpIcon from "@mui/icons-material/LiveHelp";
 import AddIcon from "@mui/icons-material/Add";
 import youtube from "../assets/youtube.png" ;
+import TurnRightIcon from '@mui/icons-material/TurnRight';
+import TurnLeftIcon from '@mui/icons-material/TurnLeft';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import OpenWithIcon from '@mui/icons-material/OpenWith';
+
 const Body = () => {
   return (
     <div className="grid gap-y-10">
@@ -36,7 +41,7 @@ const Body = () => {
          header="Story of Alignment Scope of Agility Specific Accountable Staggering Approach"
         >
           <div>
-          <img src={youtube} alt="Deeptought channel" />;
+          <img src={youtube} alt="Deeptought channel" />
           </div>
         </Board>
         <Board
@@ -85,41 +90,69 @@ const Body = () => {
           icon={<InfoIcon color="white" />}
           header="Write a 300 - 400 word article from your thread . Publish your understanding and showcase your understanding to the entire world "
         >
-          <div>
-            <div className="flex p-2 bg-yellow-100 items-center">
-              <KeyboardArrowUpIcon color="black" />
-              <h3 className="ml-16 font-bold text-lg">Thread A</h3>
+       <div className="px-5 py-7 font-semibold text-lg">
+        Title
+       </div>
+       <div className="px-4 py-2 ">
+               
+             
+       <textarea
+        className="flex-1 rounded-xl shadow-grey shadow-2xl border border-gray-200 resize-none w-96 h-full"
+       
+      >
+          
+      </textarea>
+      </div>
+      <div className="px-5 py-7 font-semibold text-lg">
+        Content
+       </div>
+      
+       <div className="px-4 py-2">
+               
+             
+       <div class="box-border h-48 w-96 p-4 border">
+       
+        <nav className="w-full flex justify-between items-center navbar">
+          <ul className="list-none sm:flex hidden justify-end items-center">
+            <div className="text-sm text-gray-400/100 font-semibold">
+            File
             </div>
-            <div className="m-8 grid grid-cols-2 gap-x-4 gap-y-4">
-              <Form label="Sub thread 1" />
-              <Form label="Sub Interpretation 1" />
-              <div className="flex justify-end items-center">
-                <div className="grid grid-cols-4 gap-2">
-                  <LightbulbIcon color="black" />
-                  <CommentIcon color="black" />
-                  <LiveHelpIcon color="black" />
-                  <LiveHelpIcon color="black" />
-                </div>
+            <div className=" relative mx-2 text-sm text-gray-400/100 font-semibold">
+              Edit
               </div>
-              <div className="grid grid-cols-2 gap-x-0.5">
-                <div className="flex justify-between items-center px-1 py-3 rounded-lg shadow-lg">
-                  <span className="text-xs font-bold">Select Categ</span>
-                  <KeyboardArrowUpIcon color="black" fontSize="14dp" />
-                </div>
-                <div className="flex justify-between items-center px-1 py-3 rounded-lg shadow-lg">
-                  <span className="text-xs font-bold">Select Proces</span>
-                  <KeyboardArrowUpIcon color="black" fontSize="14dp" />
-                </div>
+              <div className="relative mx-2 text-sm text-gray-400/100 font-semibold">
+              View
               </div>
-            </div>
-            <div className="px-8 py-4">
-              <buttton className="rounded-xl bg-blue-800 flex justify-between py-3 px-4 my-2 text-white w-fit">
-                <AddIcon />
-                <p>Sub-thread</p>
-              </buttton>
-              <Form label="Summary for Thread A" />
-            </div>
-          </div>
+              <div className="relative mx-2 text-sm text-gray-400/100 font-semibold">
+              Insert
+              </div>
+              <div className="relative mx-2 text-sm text-gray-400/100 font-semibold">
+              Format
+              </div>
+              <div className="relative mx-2 text-sm text-gray-400/100 font-semibold">
+              Tools
+              </div>
+              <div className="relative mx-2 text-sm text-gray-400/100 font-semibold">
+              Table 
+              </div>
+              <div className="text-sm text-gray-400/100 font-semibold">
+              Help
+              </div>
+          </ul>
+         
+        </nav>
+        <TurnLeftIcon color="disabled" />
+        <TurnRightIcon className="relative mx-2" color="disabled" />
+        <OpenWithIcon className="relative mx-3" color="disabled" />
+        <MoreHorizIcon color="disabled" />
+
+
+       </div>
+      </div>
+      
+     
+    
+    
         </Board>
         <Board
          title="4SA Method"
