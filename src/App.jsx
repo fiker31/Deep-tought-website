3fc451a04d977f376cfc37aedd6406839b41ad9c
Fiker Yibeltal <Fikeryibeltal31@gmail.com>
2023-06-22 09:37:49 +0300

    src file edited

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
 import styles from "./style";
-import {Navbar} from './components';
-import {Journeyboard} from './components';
+import { Navbar } from "./components";
+import { Journeyboard } from "./components";
+import Body from "./components/body";
+import NoticeBoard from "./components/noticeboard";
 
 const App = () => (
   <div>
-
-    <div className='bg-gray-200 w-full overflow-hidden'>
-     <div className={`${styles.paddingX} ${styles.flexCenter} `}>
-      
-      <div className={`${styles.boxWidth}`}>
-          <Navbar/>
-      </div>
-       </div>
-      </div>   
-       <div className={`${styles.length}`}>
-         <Journeyboard/>
-         </div>
+    <div className="bg-gray-200 w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter} `}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
         </div>
-       
+      </div>
+    </div>
+    <div className="grid grid-cols-8 gap-10">
+      <Journeyboard />
+      <div class="place-items-center col-span-6 pt-5">
+        <Body />
+      </div>
+      <NoticeBoard />
+    </div>
+  </div>
 );
 
-
-export default App
+export default App;
