f2aa123f2adad98ea2abcc3030eceedcdb99c629
Fiker Yibeltal <Fikeryibeltal31@gmail.com>
2023-06-26 09:20:57 +0300

    issue fixed

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -15,40 +15,37 @@ import OpenWithIcon from "@mui/icons-material/OpenWith";
 import SpaIcon from "@mui/icons-material/Spa";
 
 const Body = () => {
+  useEffect(() => {
+    async function fetchProjectData() {
+      try {
+        const response = await fetch(
+          "https://dev.deepthought.education/assets/uploads/files/files/others/ddugky_project.json"
+        );
+        const data = await response.json();
+        setProjectData(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchProjectData();
+  }, []);
+  const [projectData, setProjectData] = useState(null);
+  if (!projectData) {
+    return <div>Loading...</div>;
+  }
   return (
     <div className="grid gap-y-10">
-        const [projectData, setProjectData] = useState(null);
+      <div>
+        <h2>{projectData.title}</h2>
+        <p>{projectData.short_description}</p>
+        <img src={projectData.project_image} alt={projectData.title} />
+        <ul>
+          {projectData.learning_outcomes.map((outcome, index) => (
+            <li key={index}>{outcome}</li>
+          ))}
+        </ul>
+      </div>
 
-useEffect(() => {
-  async function fetchProjectData() {
-    try {
-      const response = await fetch('https://dev.deepthought.education/assets/uploads/files/files/others/ddugky_project.json');
-      const data = await response.json();
-      setProjectData(data);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-  fetchProjectData();
-}, []);
-
-if (!projectData) {
-  return <div>Loading...</div>;
-}
-
-return (
-  <div>
-    <h2>{projectData.title}</h2>
-    <p>{projectData.short_description}</p>
-    <img src={projectData.project_image} alt={projectData.title} />
-    <ul>
-      {projectData.learning_outcomes.map((outcome, index) => (
-        <li key={index}>{outcome}</li>
-      ))}
-    </ul>
-  </div>)
-  
-  
       <div className="flex justify-between items-center">
         <h1 className="text-blue-800 text-2xl font-bold">
           Technical Project Managment
