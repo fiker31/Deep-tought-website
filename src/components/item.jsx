0dd2af9621f247ab8fd69a5515f4403211871f7c
Fiker Yibeltal <Fikeryibeltal31@gmail.com>
2023-06-27 14:40:05 -0700

    add switch case

--- src/components/item.jsx
+++ src/components/item.jsx
@@ -3,12 +3,22 @@ import React, { useState, useEffect } from "react";
 import data from "../../dt.json";
 
 const item = ({data}) => {
-  const [threads, setThreads] = useState([]);
-  useEffect(() => {
-    fetch("")
-      .then((res) => res.json())
-      .then((data) => {
-        setThreads(data);
-      });
-  }, []);
+    const [component, setComponent] = useState([]);
+    const handleComponentChange = (event) => {
+        setComponent(event.target.value);
+      }
+      const displayComponent = () => {
+        switch(component) {
+          case 'video':
+            return <Video />;
+          case 'article':
+            return <Article />;
+          case 'threadbuilder':
+            return <ThreadBuilder />;
+          default:
+            return 
+            <p>Invalid Content</p>;
+        }
+      }
+
 }
